8d19d2bb8e1533a15e2b78c211f33d57a9b5f092
Liam <liam@nehoray.net>
2025-02-04 16:30:41 +0100

    Build bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react"
 import NavBar from "./components/NavBar"
-import { IStudent } from "./interfaces/studentInterface"
-import AddStudentCard from "./components/AddStudentCard"
 import { Outlet } from "react-router-dom"
 
 
