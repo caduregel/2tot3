187318de5bb9661a71c6cf64cf41b88318201d48
Liam <liam@nehoray.net>
2025-02-07 07:25:50 +0100

    removed unused variables

--- src/pages/SortStudentsPage.tsx
+++ src/pages/SortStudentsPage.tsx
@@ -2,20 +2,24 @@ import { useEffect, useState } from "react"
 
 const SortStudentsPage = () => {
 
-    const [loading, setLoading] = useState(true)
+    const [loading] = useState(true)
     const [dots, setDots] = useState("")
-    
+
     useEffect(() => {
         const interval = setInterval(() => {
-          setDots((prev) => (prev.length < 3 ? prev + "." : ""));
+            setDots((prev) => (prev.length < 3 ? prev + "." : ""));
         }, 300); // Adjust timing as needed
-    
+
         return () => clearInterval(interval);
-      }, []);
+    }, []);
 
     return (
         <div>
-            <h1 className="text-2xl">Sorting students{dots}</h1>
+            {
+                loading
+                    ? <h1 className="text-2xl">Sorting students{dots}</h1>
+                :   null
+            }
         </div>
     )
 }
