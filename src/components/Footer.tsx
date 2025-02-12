914841be8aed5ecfceee9cf4280b0abf6c571cf2
Liam <liam@nehoray.net>
2025-02-12 08:56:47 +0100

    footer copyrightu update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="mt-auto flex justify-center p-10 bg-slate-700 text-white">
-            <p>Copyright © Liam Nehoray</p>
+            <p>© 2025 Liam Nehoray</p>
         </footer>
     )
 }
